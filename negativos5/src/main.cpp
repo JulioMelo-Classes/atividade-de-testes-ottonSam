f83786c2ca09a84c1620eca069ef7ffa55b63b7e
samuel ótton <ottonsam@gmail.com>
2022-03-31 18:09:19 -0300

    contador de numeros negativos

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl;
+
 
 const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
+    int cont = 0;
+    int values[5];
+    for(int n=0; n<5; n++){
+        cin>>values[n];
+        if(values[n]<0){
+            cont++;
+        }
+    }
+    cout<<cont<<endl;
     return 0;
 }
